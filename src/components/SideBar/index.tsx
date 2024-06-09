e71d4631ecbf764bf88b36a3a711c656f70c8aee
福見 直人 <naoto.fukumi@arsaga.jp>
2024-06-09 17:13:00 +0900

    20240609.1

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,13 @@
+import '@/styles/layout/sidebar.scss'
+
+const Sidebar = () => {
+
+
+  return (
+    <aside className="l_sidebar">
+      <p>sidebar</p>
+    </aside>
+  )
+}
+
+export default Sidebar;
