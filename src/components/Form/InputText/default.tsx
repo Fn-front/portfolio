9550617f9e502f23d2350459bb078a5fa24eb0fb
福見 直人 <naoto.fukumi@arsaga.jp>
2024-06-15 17:44:39 +0900

    CSS input_text構造変更

--- src/components/Form/InputText/default.tsx
+++ src/components/Form/InputText/default.tsx
@@ -10,8 +10,10 @@ const Default = async() => {
     <>
       <h3 className='c_h3'>デフォルト</h3>
       <div className="l_component_wrapper u_mt16">
-        <label htmlFor="input" className='c_label_input_text'>入力フォーム</label>
-        <input type="text" name="input" className="c_input_text" />
+        <div className='c_input_text'>
+          <div className='c_input_text_item'><label htmlFor="input" className='c_input_text_item_label'>入力フォーム</label></div>
+          <div className='c_input_text_item'><input type="text" name="input" className="c_input_text_item_form" /></div>
+        </div>
       </div>
       <CodeBlock content={content} />
     </>
