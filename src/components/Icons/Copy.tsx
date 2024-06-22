2101a7b4de47010211199b9327b82d043df779f7
福見 直人 <naoto.fukumi@arsaga.jp>
2024-06-22 19:11:23 +0900

    コピー機能実装

--- src/components/Icons/Copy.tsx
+++ src/components/Icons/Copy.tsx
@@ -3,6 +3,7 @@ import Icon from '@mui/icons-material/ContentCopy';
 type Props = {
   color: string
   marginTop: string
+  size: string
 }
 
 export const ContentCopyIcon = (props: Props) => {
@@ -10,7 +11,8 @@ export const ContentCopyIcon = (props: Props) => {
     <Icon
       sx={{
         color: props.color ? props.color : 'inherit',
-        marginTop: props.marginTop ? props.marginTop : 0
+        marginTop: props.marginTop ? props.marginTop : 0,
+        fontSize: props.size ? props.size : 'inherit'
       }}
     />
   )
