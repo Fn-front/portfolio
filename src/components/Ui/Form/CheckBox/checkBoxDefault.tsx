091fb30fd8c10e1105906771d475df70d28d0a30
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-06 18:55:57 +0900

    チェックボックス　デフォルトコンポーネント調整

--- src/components/Ui/Form/CheckBox/checkBoxDefault.tsx
+++ src/components/Ui/Form/CheckBox/checkBoxDefault.tsx
@@ -3,19 +3,19 @@ import { getFile } from '@/features/ReadFile'
 
 const Default = async() => {
   
-  // const contentPath = '/src/styles/components/form/inputText/_default.scss'
-  // const content: string = await getFile(contentPath)
+  const contentPath = '/src/styles/components/form/checkBox/_default.scss'
+  const content: string = await getFile(contentPath)
 
   return (
     <>
       <h3 className='c_h3'>デフォルト</h3>
       <div className='l_component_wrapper u_mt16'>
-        <div className='c_input_text'>
-          <div className='c_input_text_item'><label htmlFor='input' className='c_input_text_item_label'>入力フォーム</label></div>
-          <div className='c_input_text_item'><input type='text' name='input' className='c_input_text_item_form' /></div>
+        <div className='l_form_column_2'>
+          <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
+          <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
         </div>
       </div>
-      {/* <CodeBlock content={content} /> */}
+      <CodeBlock content={content} />
     </>
   )
 }
