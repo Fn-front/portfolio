4cd606a5955e942c643076ba9caddad1f88bf0f4
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-06 19:00:24 +0900

    CSS form layout center寄せ

--- src/components/Ui/Form/CheckBox/checkBoxDefault.tsx
+++ src/components/Ui/Form/CheckBox/checkBoxDefault.tsx
@@ -10,7 +10,7 @@ const Default = async() => {
     <>
       <h3 className='c_h3'>デフォルト</h3>
       <div className='l_component_wrapper u_mt16'>
-        <div className='l_form_column_2'>
+        <div className='l_form_column_2 l_form_column_align_center'>
           <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
           <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
         </div>
