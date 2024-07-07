e9f654360b605453e05bb61579354fa6a0b01fe5
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-07 18:45:48 +0900

    html実装

--- src/components/Ui/Form/CheckBox/checkBoxList.tsx
+++ src/components/Ui/Form/CheckBox/checkBoxList.tsx
@@ -10,9 +10,25 @@ const CheckBoxList = async() => {
     <>
       <h3 className='c_h3 u_mt40'>チェックボックスリスト</h3>
       <div className='l_component_wrapper u_mt16'>
-        <div className='l_form_column_2 l_form_column_align_center'>
-          <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
-          <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+        <div className='l_check_box_list'>
+          <div className="l_check_box_list_item">
+            <div className='l_form_column_2 l_form_column_align_center'>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+            </div>
+          </div>
+          <div className="l_check_box_list_item">
+            <div className='l_form_column_2 l_form_column_align_center'>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+            </div>
+          </div>
+          <div className="l_check_box_list_item">
+            <div className='l_form_column_2 l_form_column_align_center'>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+            </div>
+          </div>
         </div>
       </div>
       {/* <CodeBlock content={content} /> */}
