5ba0f6ccd931ca3e8ee142f92559aec2d4de2cae
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-07 18:42:40 +0900

    CSS調整

--- src/components/Ui/Form/CheckBox/checkBoxList.tsx
+++ src/components/Ui/Form/CheckBox/checkBoxList.tsx
@@ -1,14 +1,14 @@
 import CodeBlock from '@/components/Ui/Code'
 import { getFile } from '@/features/ReadFile'
 
-const Default = async() => {
+const CheckBoxList = async() => {
   
   // const contentPath = '/src/styles/components/form/checkBox/_default.scss'
   // const content: string = await getFile(contentPath)
 
   return (
     <>
-      <h3 className='c_h3'>デフォルト</h3>
+      <h3 className='c_h3 u_mt40'>チェックボックスリスト</h3>
       <div className='l_component_wrapper u_mt16'>
         <div className='l_form_column_2 l_form_column_align_center'>
           <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
@@ -20,4 +20,4 @@ const Default = async() => {
   )
 }
 
-export default Default;
+export default CheckBoxList;
