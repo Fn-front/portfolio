133abcd28cd6177d5b5d2fa1945d5db026e3b515
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-07 18:53:59 +0900

    inputのidが被っていたので修正

--- src/components/Ui/Form/CheckBox/checkBoxList.tsx
+++ src/components/Ui/Form/CheckBox/checkBoxList.tsx
@@ -13,38 +13,38 @@ const CheckBoxList = async() => {
         <div className='l_check_box_list'>
           <div className="l_check_box_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
-              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox1" /></div>
               <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
             </div>
           </div>
           <div className="l_check_box_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
-              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
-              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox2" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox2' className='c_input_text_item_label'>入力フォーム</label></div>
             </div>
           </div>
           <div className="l_check_box_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
-              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
-              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox3" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox3' className='c_input_text_item_label'>入力フォーム</label></div>
             </div>
           </div>
           <div className="l_check_box_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
-              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
-              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox4" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox4' className='c_input_text_item_label'>入力フォーム</label></div>
             </div>
           </div>
           <div className="l_check_box_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
-              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
-              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox5" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox5' className='c_input_text_item_label'>入力フォーム</label></div>
             </div>
           </div>
           <div className="l_check_box_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
-              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox" /></div>
-              <div className='l_form_column_item'><label htmlFor='checkbox' className='c_input_text_item_label'>入力フォーム</label></div>
+              <div className='l_form_column_item'><input type="checkbox" className='c_check_box' name="" id="checkbox6" /></div>
+              <div className='l_form_column_item'><label htmlFor='checkbox6' className='c_input_text_item_label'>入力フォーム</label></div>
             </div>
           </div>
         </div>
