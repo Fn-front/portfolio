bbe54d34ec91c68e4dcd94370688be21f20b09ca
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-14 19:26:56 +0900

    CSS h1 レイアウト調整

--- src/app/codeView/page.tsx
+++ src/app/codeView/page.tsx
@@ -3,7 +3,9 @@ import UiForm from '@/app/codeView/@views/ui/form'
 export default function codeView() {
   return (
     <>
-      <h1 className='c_h1 u_ta_center'>UI</h1>
+      <h1 className='c_h1 u_ta_center'>
+        <span className='c_h1_inner_underline'>UI</span>
+      </h1>
       <UiForm />
     </>
   );
