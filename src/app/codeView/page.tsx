8f02e35a2ef463c1f8810552ae3e5bc0f5cb4fc7
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-14 19:18:58 +0900

    codeView h1追加

--- src/app/codeView/page.tsx
+++ src/app/codeView/page.tsx
@@ -2,6 +2,9 @@ import UiForm from '@/app/codeView/@views/ui/form'
 
 export default function codeView() {
   return (
-    <UiForm />
+    <>
+      <h1 className='c_h1'>UI</h1>
+      <UiForm />
+    </>
   );
 }
