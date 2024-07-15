c0df607d214d8a01bab2da4dd819dcc4c704b055
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-15 15:41:01 +0900

    パンくず コンポーネント実装

--- src/components/Ui/Breadcrumbs/index.tsx
+++ src/components/Ui/Breadcrumbs/index.tsx
@@ -1,5 +1,36 @@
-export const Breadcrumbs = () => {
+import Link from 'next/link';
 
+type Props = {
+  segment: string
+  segments: string[]
+  breadcrumbs: {
+    path: string;
+    segment: string;
+    breadcrumb: string;
+  }[][]
+}
+
+export const Breadcrumbs = (props: Props) => {
+
+  const { segment, segments, breadcrumbs } = props
+
+  return (
+    <>
+      {
+      segments[0] != '(TOP)' &&
+        <Link href='/'>ホーム</Link>
+      }
+      {
+        breadcrumbs.map((item, index) => {
+          return (
+            <Link key={index} href={item[0].segment !== segment ? item[0].path : ''}>
+              {item[0].breadcrumb}
+            </Link>
+          )
+        })
+      }
+    </>
+  )
 }
 
 export default Breadcrumbs
