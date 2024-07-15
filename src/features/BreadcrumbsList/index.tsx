c5e12b3aa468f9e8d6b47f35ff95d4e2b53ce513
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-15 15:41:11 +0900

    feature パンくず実装

---
+++ src/features/BreadcrumbsList/index.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useSelectedLayoutSegments } from "next/navigation"
+import Breadcrumbs from '@/components/Ui/Breadcrumbs'
+
+const routes = [
+  {
+    path: "/",
+    segment: '(TOP)',
+    breadcrumb: 'ホーム',
+  },
+  {
+    path: "codeView",
+    segment: 'codeView',
+    breadcrumb: 'コードビュー',
+  },
+  {
+    path: "codeView/ui/form/inputText",
+    segment: 'inputText',
+    breadcrumb: 'input',
+  }
+];
+
+export const BreadcrumbsList = () => {
+
+  // 現在の表示しているページのディレクトリまですべての情報
+  const segments = useSelectedLayoutSegments()
+
+  // 現在の表示しているページのディレクトリ
+  const segment = segments[segments.length -1]
+
+  // routesと比較し一致しているものを返す
+  const breadcrumb = segments.map(i => routes.filter((s) => i == s.segment )).filter(i => i.length != 0)
+
+  // mapの次のfilter：上記で一致しない場合array(0)が入っているので削除する
+  const breadcrumbs = breadcrumb.filter(i => i.length != 0)  
+  
+  return (
+    <Breadcrumbs segment={segment} segments={segments} breadcrumbs={breadcrumbs} />
+  )
+}
+
+export default BreadcrumbsList
