609e8e5a0026dacf613785afcc4540afbcbbb86f
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-20 17:09:13 +0900

    CSS radio wrapper実装

--- src/components/Ui/Form/Radio/radioDefault.tsx
+++ src/components/Ui/Form/Radio/radioDefault.tsx
@@ -11,8 +11,18 @@ const Default = async() => {
       <h3 className='c_h3'>デフォルト</h3>
       <div className='l_component_wrapper u_mt16'>
         <div className='l_form_column_2'>
-          <div className='l_form_column_item'><label htmlFor='input'></label></div>
-          <div className='l_form_column_item'><input type='text' name='radio' className='' /></div>
+          <div className='l_form_column_item'>
+            <div className="c_radio_wrapper">
+              <input type='radio' name='radio' id='radio1' className='c_radio' defaultChecked={true} />
+              <label htmlFor='radio1' className='c_radio_label'>inputテキスト</label>
+            </div>
+          </div>
+          <div className='l_form_column_item'>
+            <div className="c_radio_wrapper">
+              <input type='radio' name='radio' id='radio2' className='c_radio' />
+              <label htmlFor='radio2' className='c_radio_label'>inputテキスト</label>
+            </div>
+          </div>
         </div>
       </div>
       <CodeBlock content={content} />
