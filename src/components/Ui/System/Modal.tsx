bf339fa2940e70df13f1826330ffd4d8634f0d35
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-21 19:08:14 +0900

    Modal初期実装

---
+++ src/components/Ui/System/Modal.tsx
@@ -0,0 +1,38 @@
+import { useLayoutEffect, useState } from 'react'
+import parse from 'html-react-parser';
+import highlight from 'highlight.js';
+
+// import { renderToStaticMarkup } from 'react-dom/server'
+import  { format } from 'prettier/standalone'
+import * as parserHTML from 'prettier/parser-html';
+
+const SystemModal = (props: any) => {
+
+  const [code, setCode] = useState<string>('')
+
+  useLayoutEffect(() => {
+    (async() => {
+      if (!props.element) return
+      
+      const test = await format((props.element), {
+        parser: 'html',
+        plugins: [parserHTML],
+      })    
+      
+      const highlightedCode: string = highlight.highlight(test, {language: 'html'}).value;
+      console.log(test);
+      
+      setCode(highlightedCode)
+    })()
+  })
+  
+  
+
+  return (
+    <>
+      <pre><code className='html'>{ parse(code) }</code></pre>
+    </>
+  )
+}
+
+export default SystemModal;
