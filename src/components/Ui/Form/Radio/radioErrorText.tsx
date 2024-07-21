1bd2baf4c3e34ad6d8e3720605e6eb52296cdc30
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-21 19:30:21 +0900

    radioボタン　構造変更

--- src/components/Ui/Form/Radio/radioErrorText.tsx
+++ src/components/Ui/Form/Radio/radioErrorText.tsx
@@ -1,5 +1,6 @@
 import CodeBlock from '@/components/Ui/Code'
 import { getFile } from '@/features/ReadFile'
+import ComponentWrapper from '@/components/Layout/ComponentWrapper'
 
 const Default = async() => {
   
@@ -9,7 +10,7 @@ const Default = async() => {
   return (
     <>
       <h3 className='c_h3 u_mt40'>エラーメッセージ</h3>
-      <div className='l_component_wrapper u_mt16'>
+      <ComponentWrapper styleCode={content}>
         <div className="l_form_list">
           <div className="l_form_list_item">
             <div className='l_form_column_2 l_form_column_align_center'>
@@ -33,8 +34,7 @@ const Default = async() => {
           </div>
         </div>
         <p className='c_text_error u_mt8'>エラーテキスト</p>
-      </div>
-      <CodeBlock content={content} />
+      </ComponentWrapper>
     </>
   )
 }
