91829247966cc886eee32e80ddb2817d44f69c73
福見 直人 <naoto.fukumi@arsaga.jp>
2024-07-28 18:22:53 +0900

    codeBlock 一旦フラグで出し分け

--- src/components/Ui/Code/CodeBlock.tsx
+++ src/components/Ui/Code/CodeBlock.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Style from '@/components/Ui/Code/StyleCodeBlock'
 import Html from '@/components/Ui/Code/HtmlCodeBlock'
 
@@ -12,26 +13,41 @@ export const CodeBlock = ({
   styleCode: string
 }>
 ) => {
+
+  const [viewCodeBlock, setViewCodeBlock] = useState<boolean>(true)
+  const [viewHtml, setViewHtml] = useState<boolean>(true)
+  const [viewStyle, setViewStyle] = useState<boolean>(true)
+
   return (
     <>
-      <div className="p_code_block">
-        <ul className='l_list_icon_switch'>
-          <li className='l_list_icon_switch_item'>
-            <div className='c_icon_switch'>
-              <SwitchRightIcon fontSize='large' />
-            </div>
-          </li>
-          <li className='l_list_icon_switch_item'>
-            <div className='c_icon_switch'>
-              <SwitchLeftIcon fontSize='large' />
-            </div>
-          </li>
-        </ul>
-        <div className='p_code_block_list'>
-          <Html element={htmlCode}/>
-          <Style content={styleCode} />
+      { viewCodeBlock && (
+
+        <div className="p_code_block">
+          <ul className='l_list_icon_switch'>
+            <li className='l_list_icon_switch_item'>
+              <div
+                className='c_icon_switch'
+                onClick={() => setViewHtml((prev) => !prev)}
+              >
+                <SwitchRightIcon fontSize='large' />
+              </div>
+            </li>
+            <li className='l_list_icon_switch_item'>
+              <div
+                className='c_icon_switch'
+                onClick={() => setViewStyle((prev) => !prev)}
+              >
+                <SwitchLeftIcon fontSize='large' />
+              </div>
+            </li>
+          </ul>
+          <div className='p_code_block_list'>
+            { viewHtml && <Html element={htmlCode}/>}
+            { viewStyle && <Style content={styleCode} />}
+          </div>
         </div>
-      </div>
+        
+      ) }
     </>
   )
 }
