fb19d31159967e67b31415fdea7cdf110c247e88
Fn-front <reservoir-station@outlook.jp>
2024-08-11 18:43:04 +0900

    seeder 作成

---
+++ prisma/seeder/index.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+
+// インスタンスを生成
+const prisma = new PrismaClient()
+
+async function main() {
+  console.log('Seeding data...');
+
+  const user1 = await prisma.user.create({
+    data: {
+      id: 1,
+      title: 'ダミーテキスト１',
+      date: '2024-08-11',
+      done: false
+    },
+  });
+
+  console.log('Data seeding complete.');
+}
+
+main()
+  .catch(err => {
+    console.error('Error seeding data:', err);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
