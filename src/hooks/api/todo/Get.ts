b102b53d58d7fb51f8c1f377b298ba1377cc9580
Fn-front <reservoir-station@outlook.jp>
2024-08-12 18:10:05 +0900

    host変更

--- src/hooks/api/todo/Get.ts
+++ src/hooks/api/todo/Get.ts
@@ -1,4 +1,4 @@
-const host = process.env.LOCALHOST_URL ? process.env.LOCALHOST_URL : 'http://localhost:2937'
+const host = process.env.LOCALHOST_URL ? process.env.LOCALHOST_URL : 'http://127.0.0.1:2937'
 
 export const getList = async() => {
   const res = await fetch(host+'/api/todo')
