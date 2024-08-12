ba121aadc4d76366bec06f3c21f7b307440463a9
Fn-front <reservoir-station@outlook.jp>
2024-08-12 18:43:05 +0900

    コメント追加

--- src/app/mock/todo/page.tsx
+++ src/app/mock/todo/page.tsx
@@ -1,11 +1,14 @@
 import { getList } from '@/hooks/api/todo/Get'
 import { unstable_noStore as noStore } from 'next/cache';
 
+// localhostのfetchがgithub actionsのbuildだと動かないので記述が必要
 export const dynamic = 'force-dynamic'
 
 export default async function MockTodo() {
 
+  // github actions用
   noStore();
+  
   const data = await getList()
 
   return (
