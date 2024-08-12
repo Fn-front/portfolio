8bfbc614b8068b9123d82a98e4a4f26c72fd5763
Fn-front <reservoir-station@outlook.jp>
2024-08-12 18:44:11 +0900

    環境変数のみを参照するように変更

--- src/hooks/api/todo/Get.ts
+++ src/hooks/api/todo/Get.ts
@@ -1,4 +1,4 @@
-const host = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:2937'
+const host = process.env.NEXT_PUBLIC_API_URL
 
 export const getList = async() => {
   const res = await fetch(host+'/api/todo')
