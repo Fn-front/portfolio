a4f6d92eb3f108c8f5af35bae7244c1f4ad32c0b
Fn-front <reservoir-station@outlook.jp>
2024-08-12 16:38:34 +0900

    DB接続関数作成

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,4 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 // インスタンスを生成
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+// データベースに接続する関数
+export const connect = async () => {
+  try {
+    //prismaでデータベースに接続
+    prisma.$connect();
+  } catch (error) {
+    return Error("DB接続に失敗しました");
+  }
+};
