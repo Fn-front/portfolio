41ab760da69e7f1f629d5c2d6bf6addefbe58f80
Fn-front <reservoir-station@outlook.jp>
2024-08-13 17:55:51 +0900

    CSS 調整

--- src/app/mock/todo/page.tsx
+++ src/app/mock/todo/page.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { getList } from '@/hooks/api/todo/Get'
 import { unstable_noStore as noStore } from 'next/cache';
+import styles from './todo.module.scss'
 
 // localhostのfetchがgithub actionsのbuildだと動かないので記述が必要
 export const dynamic = 'force-dynamic'
@@ -8,16 +10,18 @@ export default async function MockTodo() {
 
   // github actions用
   noStore();
-  
+
   const data = await getList()
 
   return (
-    <ul>
+    <ul className={styles.m_todo_list}>
       { data.data.map((todo: any) => (
-        <li key={todo.id}>
-          <p>{ todo.id }</p>
-          <p>タイトル：{ todo.title }</p>
-          <span>日付：{ todo.date }</span>
+        <li key={todo.id} className={styles.m_todo_list_item}>
+          <span className={styles.m_todo_list_item_num}>{ todo.id }</span>
+          <div>
+            <span className={styles.m_todo_list_item_date}>Date：{ todo.date }</span>
+            <p>タイトル：{ todo.title }</p>
+          </div>
         </li>
       )) }
     </ul>
