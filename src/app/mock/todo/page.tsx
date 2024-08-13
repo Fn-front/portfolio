6e276109f7864fbbf7e8c7dd2db0da78abaa51e1
Fn-front <reservoir-station@outlook.jp>
2024-08-13 18:10:40 +0900

    apiをclientから呼び出し、非同期に変更

--- src/app/mock/todo/page.tsx
+++ src/app/mock/todo/page.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
 import { getList } from '@/hooks/api/todo/Get'
 import { unstable_noStore as noStore } from 'next/cache';
 import styles from './todo.module.scss'
@@ -6,16 +8,27 @@ import styles from './todo.module.scss'
 // localhostのfetchがgithub actionsのbuildだと動かないので記述が必要
 export const dynamic = 'force-dynamic'
 
-export default async function MockTodo() {
+export default function MockTodo() {
 
   // github actions用
   noStore();
 
-  const data = await getList()
+  const [dataList, setDataList] = useState<Array<object>>();
+
+  useEffect(() => {
+
+    (async() => {      
+      const data = await getList()
+      setDataList(data.data)
+    })()
+
+  }, [])
+
+
 
   return (
     <ul className={styles.m_todo_list}>
-      { data.data.map((todo: any) => (
+      { dataList && dataList.map((todo: any) => (
         <li key={todo.id} className={styles.m_todo_list_item}>
           <span className={styles.m_todo_list_item_num}>{ todo.id }</span>
           <div>
