995b0f69e574c0a860a96c285f2451cc370daae7
Fn-front <reservoir-station@outlook.jp>
2024-08-15 14:08:57 +0900

    delete型追加

--- src/hooks/api/todo/types.ts
+++ src/hooks/api/todo/types.ts
@@ -1,13 +1,17 @@
-export type addList= {
+export type addList = {
   id: number,
   title: string,
   date: string,
   done: boolean
 }
 
-export type post= {
+export type post = {
   id: number,
   title: string,
   date: string,
   done: boolean
+}
+
+export type deleteData = {
+  id: Array<number>
 }
