710372affd3de7c3b5f8b9f7a487a20a158e3121
Fn-front <reservoir-station@outlook.jp>
2024-08-15 14:44:44 +0900

    delete API 実装

--- src/hooks/api/todo/Delete.ts
+++ src/hooks/api/todo/Delete.ts
@@ -1,3 +1,16 @@
 const host = process.env.NEXT_PUBLIC_API_URL
+import { deleteList } from './types'
 
-export const deleteData = () => {}
+export const deleteData = async(data: deleteList) => {
+  const res = await fetch(host+'/api/todo', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: data
+    })
+  })
+
+  return res.json()
+}
