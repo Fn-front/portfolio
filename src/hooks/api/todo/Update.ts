4a1015204f670923c74e0b23ed9b32031a6ca031
Fn-front <reservoir-station@outlook.jp>
2024-08-18 11:53:45 +0900

    hooks update追加

---
+++ src/hooks/api/todo/Update.ts
@@ -0,0 +1,19 @@
+import { post } from './types'
+
+export const update = (data: post) => {
+  const { id, title, date, done } = data
+  const res = fetch('/api/todo', {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+        id: id,
+        title: title,
+        date: date,
+        done: done
+    })
+  })
+
+  return res
+}
