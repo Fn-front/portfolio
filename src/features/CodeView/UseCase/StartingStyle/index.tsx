095ddfd19569458d159b5ebc955301765ffb3a35
Fn-front <reservoir-station@outlook.jp>
2024-08-24 20:21:55 +0900

    Transition追加

--- src/features/CodeView/UseCase/StartingStyle/index.tsx
+++ src/features/CodeView/UseCase/StartingStyle/index.tsx
@@ -1,6 +1,7 @@
 import { getFile } from '@/features/ReadFile'
 import ComponentWrapper from '@/components/Layout/ComponentWrapper'
 import Animation from './Animation'
+import Transition from './Transition'
 
 export const StartingStyle = async() => {
   
@@ -12,6 +13,10 @@ export const StartingStyle = async() => {
       <h3 className='c_h3'>animationは、display: noneでも動く</h3>
       <ComponentWrapper styleCode={content}>
         <Animation />
+      </ComponentWrapper>
+      <h3 className='c_h3 u_mt40'>Transition @starting-style 設定あり</h3>
+      <ComponentWrapper styleCode={content}>
+        <Transition />
       </ComponentWrapper>
     </>
   )
