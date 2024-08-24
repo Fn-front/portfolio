f3355e2233b3bc1fa8b49c5e7cf76d81a3ea010d
Fn-front <reservoir-station@outlook.jp>
2024-08-24 20:26:16 +0900

    animationとtransitionを追加

--- src/features/CodeView/UseCase/StartingStyle/index.tsx
+++ src/features/CodeView/UseCase/StartingStyle/index.tsx
@@ -5,17 +5,20 @@ import Transition from './Transition'
 
 export const StartingStyle = async() => {
   
-  const contentPath = '/src/styles/project/useCase/startingStyle/_starting_style_animation.scss'
-  const content: string = await getFile(contentPath)
+  const contentPath1 = '/src/styles/project/useCase/startingStyle/_starting_style_animation.scss'
+  const content1: string = await getFile(contentPath1)
+
+  const contentPath2 = '/src/styles/project/useCase/startingStyle/_starting_style_transition.scss'
+  const content2: string = await getFile(contentPath2)
 
   return (
     <>
       <h3 className='c_h3'>animationは、display: noneでも動く</h3>
-      <ComponentWrapper styleCode={content}>
+      <ComponentWrapper styleCode={content1}>
         <Animation />
       </ComponentWrapper>
       <h3 className='c_h3 u_mt40'>Transition @starting-style 設定あり</h3>
-      <ComponentWrapper styleCode={content}>
+      <ComponentWrapper styleCode={content2}>
         <Transition />
       </ComponentWrapper>
     </>
