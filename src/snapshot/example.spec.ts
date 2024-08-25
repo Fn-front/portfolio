31a25c30d316500592ca3028ef54de86d5c67662
Fn-front <reservoir-station@outlook.jp>
2024-08-25 18:57:05 +0900

    playwright snapshot checkbox

--- src/snapshot/example.spec.ts
+++ src/snapshot/example.spec.ts
@@ -1,18 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Screen Shots', async ({ page }) => {
+  await page.goto('http://localhost:2937/codeView/ui/form/checkBox/');
+  await page.screenshot({ path: "public/assets/img/ui/checkBox.png" });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
