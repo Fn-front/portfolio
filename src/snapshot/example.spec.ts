c29c260abcf7a5e17aa8655c14e95bb235b35022
Fn-front <reservoir-station@outlook.jp>
2024-08-25 18:58:45 +0900

    playwright パラレル タスク分割

--- src/snapshot/example.spec.ts
+++ src/snapshot/example.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
+test.describe.configure({ mode: 'parallel' });
 
-test('Screen Shots', async ({ page }) => {
+test('Screen Shot checkBox', async ({ page }) => {
   await page.goto('http://localhost:2937/codeView/ui/form/checkBox/');
   await page.screenshot({ path: "public/assets/img/ui/checkBox.png" });
-
 });
