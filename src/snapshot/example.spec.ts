8732a86a426b38cedb1e7e4da0279a8decb28402
Fn-front <reservoir-station@outlook.jp>
2024-08-25 20:05:22 +0900

    snapshot 生成位置調整

--- src/snapshot/example.spec.ts
+++ src/snapshot/example.spec.ts
@@ -3,9 +3,9 @@ test.describe.configure({ mode: 'parallel' });
 
 test('Screen Shot checkBox', async ({ page }) => {
   await page.goto('http://localhost:2937/codeView/ui/form/checkBox/');
-  await page.screenshot({ path: "public/assets/img/codeView/ui/form/checkBox.png" });
+  await page.screenshot({ path: "public/assets/img/codeView/ui/form/checkBox.png", clip: { x: 200, y: 80, width: 1920, height: 1080 } });
 });
 test('Screen Shot inputText', async ({ page }) => {
   await page.goto('http://localhost:2937/codeView/ui/form/inputText/');
-  await page.screenshot({ path: "public/assets/img/codeView/ui/form/inputText.png" });
+  await page.screenshot({ path: "public/assets/img/codeView/ui/form/inputText.png", clip: { x: 200, y: 80, width: 1920, height: 1080 } });
 });
