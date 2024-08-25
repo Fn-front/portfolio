c08d14056df79374cfe035d4be29302919027e00
Fn-front <reservoir-station@outlook.jp>
2024-08-25 19:17:15 +0900

    生成フォルダ ディレクトリ変更

--- src/snapshot/example.spec.ts
+++ src/snapshot/example.spec.ts
@@ -3,9 +3,9 @@ test.describe.configure({ mode: 'parallel' });
 
 test('Screen Shot checkBox', async ({ page }) => {
   await page.goto('http://localhost:2937/codeView/ui/form/checkBox/');
-  await page.screenshot({ path: "public/assets/img/codeView/ui/checkBox.png" });
+  await page.screenshot({ path: "public/assets/img/codeView/ui/form/checkBox.png" });
 });
 test('Screen Shot inputText', async ({ page }) => {
   await page.goto('http://localhost:2937/codeView/ui/form/inputText/');
-  await page.screenshot({ path: "public/assets/img/codeView/ui/inputText.png" });
+  await page.screenshot({ path: "public/assets/img/codeView/ui/form/inputText.png" });
 });
