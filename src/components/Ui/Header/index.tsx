1ca7c2c7d9a3b8a48fc695070c0b8a2aadfd92ce
Fn-front <reservoir-station@outlook.jp>
2024-08-25 19:09:23 +0900

    header sidebar クライアント処理削除に伴いserver componentに変更

--- src/components/Ui/Header/index.tsx
+++ src/components/Ui/Header/index.tsx
@@ -1,12 +1,4 @@
-'use client'
-
-import { useSearchParams } from "next/navigation";
-
 const Header = () => {
-
-  // クエリパラメータにiframeが含まれていたらheaderを非表示
-  const searchParams = useSearchParams();
-  const getParams = searchParams.get('iframe')
 
   return (
     <header className={`l_header`}></header>
--- src/components/Ui/SideBar/index.tsx
+++ src/components/Ui/SideBar/index.tsx
@@ -1,12 +1,4 @@
-'use client'
-
-import { useSearchParams } from "next/navigation";
-
 const Sidebar = () => {
-
-  // クエリパラメータにiframeが含まれていたらsidebarを非表示
-  const searchParams = useSearchParams();
-  const getParams = searchParams.get('iframe')
 
   return (
     <aside className={`l_sidebar`}>
