3f2471e0190599f66ec147f33800df44bb04ddcc
Fn-front <reservoir-station@outlook.jp>
2024-08-31 20:39:47 +0900

    auth layout 調整

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,13 @@ export default function RootLayout({
 
   return (
     <>
-      { children }
+      <div className='l_global_container'>
+        <main className='l_main'>
+          <div className='l_container'>
+            {children}
+          </div>
+        </main>
+      </div>
     </>
   );
 }
