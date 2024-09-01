f59c7d5e14551c7e1a1fd6a488ae26361b4f5756
Fn-front <reservoir-station@outlook.jp>
2024-09-01 18:42:29 +0900

    otherから変更

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <>
       <main className='l_main'>
-        <div className='l_container_other'>
+        <div className='l_container'>
           {children}
         </div>
       </main>
