3460b5c21ac41f76c8100acffe25726833e1bafd
Fn-front <reservoir-station@outlook.jp>
2024-09-01 18:54:38 +0900

    HeadLineをコンポーネント化

---
+++ src/components/Ui/HeadLine/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+type Props = {
+  Component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
+  label: string
+}
+
+export const HeadLine: FC<Props> = ({ Component = 'h1', label }) => {
+  return <Component className={`c_${Component}`}>{ label }</Component>
+}
+
+export default HeadLine
