c1f6c22d9b1b5370cca69945836427881d970672
Fn-front <reservoir-station@outlook.jp>
2024-09-01 17:46:07 +0900

    ログイン 入力フォーム name 作成

--- src/app/(auth)/user/login/page.tsx
+++ src/app/(auth)/user/login/page.tsx
@@ -1,10 +1,25 @@
-import LayoutSign from '@/components/Layout/Auth/Sign'
+'use client'
 
-export default function login() {
+import LayoutSign from '@/components/Layout/Auth/Sign'
+import InputText from '@/components/Ui/Form/InputText'
+import AuthFormComponent from '@/components/Layout/Auth/Form'
+import { useForm } from 'react-hook-form';
+
+export default function Login() {
+
+  const { register, handleSubmit } = useForm()
+
   return (
     <>
       <LayoutSign>
-        <p>ログイン</p>
+        <h2 className='c_h2'>ログイン</h2>
+        <AuthFormComponent mt="32">
+          <InputText
+            label="name"
+            placeholder='user name'
+            {...register('name')}
+          />
+        </AuthFormComponent>
       </LayoutSign>
     </>
   );
