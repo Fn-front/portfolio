fba78ea191a70430b83d6aeb9d9be647905d4df5
Fn-front <reservoir-station@outlook.jp>
2024-09-01 18:15:17 +0900

    エラー文言 constantsを参照するように変更

--- src/components/Ui/Form/InputText/index.tsx
+++ src/components/Ui/Form/InputText/index.tsx
@@ -1,4 +1,5 @@
 import { forwardRef, ComponentPropsWithoutRef } from 'react'
+import { errorMessageInputText } from '@/functions/constants/schema'
 
 type props = {
   label: string
@@ -9,7 +10,7 @@ type Props = ComponentPropsWithoutRef<'input'> & props;
 
 const InputText = forwardRef<HTMLInputElement, Props>(
   ({label, error, ...register}, ref) => {  
-    
+  
   return (
     <>
       <label htmlFor={label} className='c_label'>{ label }</label>
@@ -20,7 +21,7 @@ const InputText = forwardRef<HTMLInputElement, Props>(
         ref={ref}
       />
       { error && 
-        <p className='c_text_error u_mt8'>{ error.message }</p>
+        <p className='c_text_error u_mt8'>{ label + errorMessageInputText }</p>
       }
     </>
   );
