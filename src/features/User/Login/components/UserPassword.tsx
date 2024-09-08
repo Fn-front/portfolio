2ff737de26456336010208f0567bb7e73da167bd
Fn-front <reservoir-station@outlook.jp>
2024-09-08 16:58:20 +0900

    component userPassword作成

---
+++ src/features/User/Login/components/UserPassword.tsx
@@ -0,0 +1,42 @@
+import InputText from '@/components/Ui/Form/InputText'
+import AuthFormComponent from '@/components/Layout/Auth/Form'
+import { errorMessageInputText } from '@/functions/constants/schema'
+import { useForm } from 'react-hook-form';
+
+export const UserPassword = () => {
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: {errors} 
+  } = useForm({
+    mode: 'onBlur',
+    criteriaMode: 'all',
+  })
+
+  return (
+    <>
+      <AuthFormComponent mt="32">
+        <InputText
+          label="password"
+          error={errors.pass}
+          placeholder='user password'
+          {...register('pass', 
+            {
+              required: {
+                value: true,
+                message: 'password' + errorMessageInputText,
+              },
+              minLength: {
+                value: 8,
+                message: '8文字以上入力してください。',
+              },
+            }
+          )}
+        />
+      </AuthFormComponent>
+    </>
+  )
+}
+
+export default UserPassword;
--- src/features/User/Login/index.tsx
+++ src/features/User/Login/index.tsx
@@ -1,44 +1,12 @@
 import LayoutSign from '@/components/Layout/Auth/Sign'
-import InputText from '@/components/Ui/Form/InputText'
-import AuthFormComponent from '@/components/Layout/Auth/Form'
 import HeadLine from '@/components/Ui/HeadLine'
-import { errorMessageInputText } from '@/functions/constants/schema'
-import { useForm } from 'react-hook-form';
 
 export const L = () => {
-
-  const { 
-    register, 
-    handleSubmit, 
-    formState: {errors} 
-  } = useForm({
-    mode: 'onBlur',
-    criteriaMode: 'all',
-  })
 
   return (
     <>
       <LayoutSign>
         <HeadLine Component='h2' label="ログイン" />
-        <AuthFormComponent mt="32">
-          <InputText
-            label="password"
-            error={errors.pass}
-            placeholder='user password'
-            {...register('pass', 
-              {
-                required: {
-                  value: true,
-                  message: 'password' + errorMessageInputText,
-                },
-                minLength: {
-                  value: 8,
-                  message: '8文字以上入力してください。',
-                },
-              }
-            )}
-          />
-        </AuthFormComponent>
       </LayoutSign>
     </>
   )
