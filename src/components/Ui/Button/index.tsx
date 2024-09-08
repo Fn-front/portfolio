2a053a925d8ebfe7135be125463e5919a5b2b081
Fn-front <reservoir-station@outlook.jp>
2024-09-08 19:15:41 +0900

    buttonコンポーネント position追加

--- src/components/Ui/Button/index.tsx
+++ src/components/Ui/Button/index.tsx
@@ -6,25 +6,28 @@ type Type = 'button' | 'submit'
 
 type Size = 'small' | 'medium' | 'large'
 
+type Position = 'center' | 'right'
+
 type Props = {
   type?: Type
   label: string
   color?: Color
   size?: Size
   mt?: Margin
+  position?: Position
 }
 
 export const Button = (props: Props) => {
 
-  const { type, label, color, size, mt } = props
+  const { type, label, color, size, mt, position } = props
 
   return (
     <>
       <button
-        type={ type ?? 'button' }
-        className={`c_button c_button_${ color ? color : 'normal'} c_button_${ size ? size : 'medium'} ${ mt ? 'u_mt'+mt : '' }`}
+        type={type ?? 'button'}
+        className={`c_button c_button_${color ? color : 'normal'} c_button_${size ? size : 'medium'} ${mt ? 'u_mt' + mt : ''} ${position ? 'c_button_' + position : ''}`}
       >
-        { label }
+        {label}
       </button>
     </>
   )
