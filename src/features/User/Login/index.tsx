dd71d1269d5acc5fda552fb9167768ad1edcf185
Fn-front <reservoir-station@outlook.jp>
2024-09-08 16:59:39 +0900

    import追加

--- src/features/User/Login/index.tsx
+++ src/features/User/Login/index.tsx
@@ -1,12 +1,16 @@
 import LayoutSign from '@/components/Layout/Auth/Sign'
 import HeadLine from '@/components/Ui/HeadLine'
+import UserName from './components/UserName'
+import UserPassword from './components/UserPassword'
 
-export const L = () => {
+export const Login = () => {
 
   return (
     <>
       <LayoutSign>
         <HeadLine Component='h2' label="ログイン" />
+        <UserName />
+        <UserPassword />
       </LayoutSign>
     </>
   )
