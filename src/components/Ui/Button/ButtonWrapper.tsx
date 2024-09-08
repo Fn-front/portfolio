3a3f72dd27c8e0106ee22150d3e18ccc5a70d170
Fn-front <reservoir-station@outlook.jp>
2024-09-08 19:08:52 +0900

    コンポーネント buttonWrapper作成

---
+++ src/components/Ui/Button/ButtonWrapper.tsx
@@ -0,0 +1,13 @@
+type Type = 'center' | 'around' | 'between'
+
+type Props = {
+  type: Type
+}
+
+export const ButtonWrapper = (props: Props) => {
+  return (
+    <></>
+  )
+}
+
+export default ButtonWrapper
