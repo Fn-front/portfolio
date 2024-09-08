156da273dd36a5a478be6361d25da7874ac15c70
Fn-front <reservoir-station@outlook.jp>
2024-09-08 18:36:33 +0900

    ボタンコンポーネント size追加

--- src/components/Ui/Button/index.tsx
+++ src/components/Ui/Button/index.tsx
@@ -2,21 +2,24 @@ type Color = 'blue' | 'green'
 
 type Type = 'button' | 'submit'
 
+type Size = 'small' | 'medium' | 'large'
+
 type Props = {
   type?: Type
   label: string
   color?: Color
+  size?: Size
 }
 
 export const Button = (props: Props) => {
 
-  const { type, label, color } = props
+  const { type, label, color, size } = props
 
   return (
     <>
       <button
         type={ type ?? 'button' }
-        className={`c_button c_button_${ color ? color : 'normal' }`}
+        className={`c_button c_button_${ color ? color : 'normal'} c_button_${ size ? size : 'medium'}`}
       >
         { label }
       </button>
