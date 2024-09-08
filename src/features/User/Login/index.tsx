6e123b7b49986c408bb1c45e2b5e7dad7b274632
Fn-front <reservoir-station@outlook.jp>
2024-09-08 17:52:30 +0900

    handleSubmit実装

--- src/features/User/Login/index.tsx
+++ src/features/User/Login/index.tsx
@@ -1,16 +1,73 @@
+'use client'
+
 import LayoutSign from '@/components/Layout/Auth/Sign'
 import HeadLine from '@/components/Ui/HeadLine'
-import UserName from './components/UserName'
-import UserPassword from './components/UserPassword'
+import InputText from '@/components/Ui/Form/InputText'
+import AuthFormComponent from '@/components/Layout/Auth/Form'
+import { errorMessageInputText, errorMessageMaxLength } from '@/functions/constants/schema'
+
+import { useForm } from 'react-hook-form';
 
 export const UserLogin = () => {
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: {errors} 
+  } = useForm({
+    mode: 'onBlur',
+    criteriaMode: 'all',
+  })
+
+  const handleFormSubmit = (data: any) => {
+    console.log(data);
+  }
 
   return (
     <>
       <LayoutSign>
         <HeadLine Component='h2' label="ログイン" />
-        <UserName />
-        <UserPassword />
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <AuthFormComponent mt="32">
+            <InputText
+              label="name"
+              error={errors.name}
+              placeholder='user name'
+              {...register('name', 
+                {
+                  required: {
+                    value: true,
+                    message: 'name' + errorMessageInputText,
+                  },
+                  minLength: {
+                    value: 8,
+                    message: '8' + errorMessageMaxLength,
+                  },
+                }
+              )}
+            />
+          </AuthFormComponent>
+          <AuthFormComponent mt="32">
+            <InputText
+              label="password"
+              error={errors.pass}
+              placeholder='user password'
+              {...register('pass', 
+                {
+                  required: {
+                    value: true,
+                    message: 'password' + errorMessageInputText,
+                  },
+                  minLength: {
+                    value: 8,
+                    message: '8' + errorMessageMaxLength,
+                  },
+                }
+              )}
+            />
+          </AuthFormComponent>
+          <button type='submit'>送信</button>
+        </form>
       </LayoutSign>
     </>
   )
