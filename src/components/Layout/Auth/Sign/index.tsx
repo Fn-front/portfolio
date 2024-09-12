1e7b0f22508da8c9ea4e70495888ba5164ccdfc5
Fn-front <reservoir-station@outlook.jp>
2024-09-12 15:04:22 +0900

    test: reviewdog

--- src/components/Layout/Auth/Sign/index.tsx
+++ src/components/Layout/Auth/Sign/index.tsx
@@ -4,7 +4,7 @@ export const AuthSign = ({
   children: React.ReactNode
 }>) => {
   return (
-    <div className='l_form_sign'>
+    <div className="l_form_sign">
       <div className='l_form_sign_inner'>{children}</div>
     </div>
   )
