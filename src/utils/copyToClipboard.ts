2f91c7c6abd502f20419175b704decf97d2dd597
Fn-front <reservoir-station@outlook.jp>
2024-09-12 11:18:43 +0900

    clean: format

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,22 +1,22 @@
 export const handleCopyToClipboard = (element: HTMLElement) => {
-  let ranges = [];
-  let selection = window.getSelection();
-  let range = document.createRange();
-  let result = false;
+  let ranges = []
+  let selection = window.getSelection()
+  let range = document.createRange()
+  let result = false
 
-  for(let i = 0; i < selection!.rangeCount; i += 1) {
-      ranges[i] = selection!.getRangeAt(i);
+  for (let i = 0; i < selection!.rangeCount; i += 1) {
+    ranges[i] = selection!.getRangeAt(i)
   }
 
-  range.selectNodeContents(element);
-  selection!.removeAllRanges();
-  selection!.addRange(range);
-  result = document.execCommand("copy");
-  selection!.removeAllRanges();
+  range.selectNodeContents(element)
+  selection!.removeAllRanges()
+  selection!.addRange(range)
+  result = document.execCommand('copy')
+  selection!.removeAllRanges()
 
-  for(let i = 0; i < ranges.length; i += 1) {
-      selection!.addRange(ranges[i]);
+  for (let i = 0; i < ranges.length; i += 1) {
+    selection!.addRange(ranges[i])
   }
 
-  return result;
-};
+  return result
+}
--- src/utils/editFilePaths.ts
+++ src/utils/editFilePaths.ts
@@ -1,7 +1,6 @@
 export const deleteAuthenticated = (path: string) => {
   const deleteString = '(authenticated)'
   const result = path.replace(deleteString, '')
-  
+
   return result
 }
-
