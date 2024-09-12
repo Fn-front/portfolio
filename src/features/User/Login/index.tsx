b7c56aac84321cc39f7c53c87f9ed42d8de8cdf9
Fn-front <reservoir-station@outlook.jp>
2024-09-12 11:15:27 +0900

    clean: format features

--- src/features/User/Login/index.tsx
+++ src/features/User/Login/index.tsx
@@ -6,72 +6,62 @@ import InputText from '@/components/Ui/Form/InputText'
 import AuthFormComponent from '@/components/Layout/Auth/Form'
 import { errorMessageInputText, errorMessageMaxLength } from '@/functions/constants/schema'
 import Button from '@/components/Ui/Button'
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'
 
 export const UserLogin = () => {
-
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     mode: 'onBlur',
     criteriaMode: 'all',
   })
 
   const handleFormSubmit = (data: any) => {
-    console.log(data);
+    console.log(data)
   }
 
   return (
     <>
       <LayoutSign>
-        <HeadLine Component='h2' label="ログイン" />
+        <HeadLine Component='h2' label='ログイン' />
         <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <AuthFormComponent mt="32">
+          <AuthFormComponent mt='32'>
             <InputText
-              label="name"
+              label='name'
               error={errors.name}
               placeholder='user name'
-              {...register('name',
-                {
-                  required: {
-                    value: true,
-                    message: 'name' + errorMessageInputText,
-                  },
-                  minLength: {
-                    value: 8,
-                    message: '8' + errorMessageMaxLength,
-                  },
-                }
-              )}
+              {...register('name', {
+                required: {
+                  value: true,
+                  message: 'name' + errorMessageInputText,
+                },
+                minLength: {
+                  value: 8,
+                  message: '8' + errorMessageMaxLength,
+                },
+              })}
             />
           </AuthFormComponent>
-          <AuthFormComponent mt="32">
+          <AuthFormComponent mt='32'>
             <InputText
-              label="password"
+              label='password'
               error={errors.pass}
               placeholder='user password'
-              {...register('pass',
-                {
-                  required: {
-                    value: true,
-                    message: 'password' + errorMessageInputText,
-                  },
-                  minLength: {
-                    value: 8,
-                    message: '8' + errorMessageMaxLength,
-                  },
-                }
-              )}
+              {...register('pass', {
+                required: {
+                  value: true,
+                  message: 'password' + errorMessageInputText,
+                },
+                minLength: {
+                  value: 8,
+                  message: '8' + errorMessageMaxLength,
+                },
+              })}
             />
           </AuthFormComponent>
-          <Button
-            type='submit'
-            label='送信'
-            mt='32'
-            position='center'
-          />
+          <Button type='submit' label='送信' mt='32' position='center' />
         </form>
       </LayoutSign>
     </>
