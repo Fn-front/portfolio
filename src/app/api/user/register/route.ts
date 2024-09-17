f48454c395301ad6eca461b96505318c2cb14180
Fn-front <reservoir-station@outlook.jp>
2024-09-17 17:11:29 +0900

    add: api user register base

---
+++ src/app/api/user/register/route.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+// biome-ignore lint:
+export const POST = async (req: Request) => {
+  try {
+    // biome-ignore lint:
+  } catch (error) {}
+};
