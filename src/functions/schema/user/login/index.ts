ce92e35d5228821353fd997d2c0274a398dbb2e9
Fn-front <reservoir-station@outlook.jp>
2024-09-17 16:39:22 +0900

    update: zod message

--- src/functions/schema/user/login/index.ts
+++ src/functions/schema/user/login/index.ts
@@ -1,8 +1,19 @@
 import { z } from 'zod';
+import {
+  errorMessageMaxLength,
+  errorMessageInputText,
+  errorMessageAlphanumeric,
+} from '@/functions/constants/schema';
 
 export const login = z.object({
-  name: z.string().min(8),
-  password: z.string().min(8),
+  name: z
+    .string()
+    .min(8, '8' + errorMessageMaxLength)
+    .regex(/^[a-zA-Z0-9]+$/, errorMessageAlphanumeric),
+  password: z
+    .string()
+    .min(8, '8' + errorMessageMaxLength)
+    .regex(/^[a-zA-Z0-9]+$/, errorMessageAlphanumeric),
 });
 
 export type loginType = z.infer<typeof login>;
