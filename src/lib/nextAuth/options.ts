aa0370fb44fd612bfb297c8b18af1e32724d9ab8
Fn-front <reservoir-station@outlook.jp>
2024-09-17 21:06:46 +0900

    update: nextauth options

---
+++ src/lib/nextAuth/options.ts
@@ -0,0 +1,66 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import bcrypt from 'bcrypt';
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+// googleとgithubでのログイン実装時に活性化
+// import GitHubProvider from 'next-auth/providers/github'
+// import GoogleProvider from 'next-auth/providers/google'
+
+import prisma from '@/lib/prisma/Prisma';
+
+const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Eメール',
+          type: 'text',
+        },
+        password: {
+          label: 'パスワード',
+          type: 'password',
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error('Email and password required');
+        }
+
+        // emailを使用しユーザーを取得
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        // ユーザーかパスワードがない場合にエラーを返す
+        if (!user || !user.password) {
+          throw new Error('Eメールが一致しません');
+        }
+
+        // パスワードのハッシュをデコードし確認
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+
+        // パスワードが一致しない場合はエラーを返す
+        if (!isCorrectPassword) {
+          throw new Error('パスワードが一致しません');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default options;
