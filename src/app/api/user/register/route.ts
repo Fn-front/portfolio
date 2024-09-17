5c1f340ecc8614755ed8b59a3085f807ac8541c4
Fn-front <reservoir-station@outlook.jp>
2024-09-17 20:06:47 +0900

    add: prisma create user

--- src/app/api/user/register/route.ts
+++ src/app/api/user/register/route.ts
@@ -1,9 +1,38 @@
 import bcrypt from 'bcrypt';
 import { NextResponse } from 'next/server';
+import prisma from '@/lib/Prisma';
 
-// biome-ignore lint:
+// ユーザー新規登録
 export const POST = async (req: Request) => {
   try {
-    // biome-ignore lint:
-  } catch (error) {}
+    const { name, email, password } = await req.json();
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    // 既存のユーザーかどうか確認
+    if (existingUser)
+      return NextResponse.json(
+        { message: '入力されたEmailはすでに存在しています' },
+        { status: 422 },
+      );
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // ユーザー登録prisma
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+        image: '',
+        emailVerified: new Date(),
+      },
+    });
+
+    return NextResponse.json({ user }, { status: 201 });
+
+    // biome-ignore lint: error
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 };
