5187c6081a82f3f9f6c21812b97516e7a556b24a
Fn-front <reservoir-station@outlook.jp>
2024-09-18 20:16:57 +0900

    update: component input radio

--- src/components/Ui/Form/Radio/index.tsx
+++ src/components/Ui/Form/Radio/index.tsx
@@ -10,18 +10,34 @@ type props = {
 type Props = ComponentPropsWithoutRef<'input'> & props;
 
 const InputText = forwardRef<HTMLInputElement, Props>(
-  ({ label, error, ...register }, ref) => {
+  ({ label, error, data, ...register }, ref) => {
     return (
       <>
-        <label htmlFor={label} className='c_label'>
-          {label}
-        </label>
-        <input
-          type='text'
-          className='c_input_text u_mt8'
-          {...register}
-          ref={ref}
-        />
+        <p className='c_label'>{label}</p>
+        <div className='l_form_list u_mt8'>
+          {data.map((item, index) => {
+            return (
+              <div className='l_form_list_item'>
+                <div className='l_form_column_2 l_form_column_align_center'>
+                  <div className='l_form_column_item' key={index}>
+                    <input
+                      type='radio'
+                      className='c_radio'
+                      value={item}
+                      {...register}
+                      ref={ref}
+                    />
+                  </div>
+                  <div className='l_form_column_item'>
+                    <label htmlFor={item}>
+                      {item}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
         {error && <p className='c_text_error u_mt8'>{error.message}</p>}
       </>
     );
