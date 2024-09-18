037bab51946b1789a4d405ff3d5c16fe22fcae40
Fn-front <reservoir-station@outlook.jp>
2024-09-18 19:50:50 +0900

    add: component radio

---
+++ src/components/Ui/Form/Radio/index.tsx
@@ -0,0 +1,33 @@
+import { forwardRef, ComponentPropsWithoutRef } from 'react';
+import { type FieldError } from 'react-hook-form';
+
+type props = {
+  label: string;
+  error: FieldError | undefined;
+  data: Array<string>
+};
+
+type Props = ComponentPropsWithoutRef<'input'> & props;
+
+const InputText = forwardRef<HTMLInputElement, Props>(
+  ({ label, error, ...register }, ref) => {
+    return (
+      <>
+        <label htmlFor={label} className='c_label'>
+          {label}
+        </label>
+        <input
+          type='text'
+          className='c_input_text u_mt8'
+          {...register}
+          ref={ref}
+        />
+        {error && <p className='c_text_error u_mt8'>{error.message}</p>}
+      </>
+    );
+  },
+);
+
+InputText.displayName = 'InputText';
+
+export default InputText;
