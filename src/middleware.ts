699c79e2c593b3027b4ba851297aee57c34a0133
Fn-front <reservoir-station@outlook.jp>
2024-09-19 21:35:47 +0900

    add: middleware admin以外ユーザー登録できないように

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,37 +2,35 @@ import { NextResponse } from 'next/server';
 import { withAuth } from 'next-auth/middleware';
 
 // 管理者権限が必要なパス一覧
-// const adminOnlyPathRegex = [
-//   new RegExp(`^/user/login/register.*$`),
-//   new RegExp(`^/xxx/.*$`),
-//   new RegExp(`^/xxx/.*$`),
-//   new RegExp(`^/xxx/.*$`),
-// ];
+const adminOnlyPathRegex = [
+  new RegExp(`^/user/register.*$`),
+  new RegExp(`^/xxx/.*$`),
+  new RegExp(`^/xxx/.*$`),
+  new RegExp(`^/xxx/.*$`),
+];
 
 export default withAuth(
-  // function middleware(req) {
-  //   const { pathname } = req.nextUrl;
+  function middleware(req) {
+    const { pathname } = req.nextUrl;
 
-  //   // 管理者権限のないユーザーが管理者権限が必要なパスにアクセスした場合は404
-  //   if (
-  //     req.nextauth.token?.userRole !== 'admin' &&
-  //     adminOnlyPathRegex.some((regex) => regex.test(pathname))
-  //   ) {
-  //     return NextResponse.rewrite(new URL('/404', req.url));
-  //   }
-  // },
+    // 管理者権限のないユーザーが管理者権限が必要なパスにアクセスした場合は404
+    if (
+      req.nextauth.token?.userRole !== 'admin' &&
+      adminOnlyPathRegex.some((regex) => regex.test(pathname))
+    ) {
+      return NextResponse.rewrite(new URL('/404', req.url));
+    }
+  },
   {
     callbacks: {
-      // 認可に関する処理。ロールが `admin` ならOK
-      async authorized({ token }) {
-        return token?.role === 'user';
+      authorized: ({ token }) => {
+        if (token) {
+          return true;
+        } else {
+          return false;
+        }
       },
     },
-    // callbacks: {
-    //   authorized: ({ token }) => {
-    //     if (token) return true;
-    //   },
-    // },
     // リダイレクトページ
     pages: {
       signIn: '/user/login',
