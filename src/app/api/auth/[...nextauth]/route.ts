45400b59e5b6f4f29d76e3eb4a8d51ca6b47d2a6
Fn-front <reservoir-station@outlook.jp>
2024-09-19 19:47:37 +0900

    update: api next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth/next';
 
-// import options from '@/lib/nextAuth/options'
+import options from '@/lib/nextAuth/options';
 
-// const handler = NextAuth(options)
+const handler = NextAuth(options);
 
-// export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
