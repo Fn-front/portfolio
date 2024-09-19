db1c4534bac88aaa9ec4163ce240e50024cee4f1
Fn-front <reservoir-station@outlook.jp>
2024-09-19 21:24:23 +0900

    update: role

--- src/functions/types/next-auth.d.ts
+++ src/functions/types/next-auth.d.ts
@@ -3,12 +3,15 @@ import 'next-auth/jwt';
 declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
-      role: 'admin' | 'user' | 'mock' | 'codeView';
+      role: string;
     } & DefaultSession['user'];
+  }
+  interface User extends DefaultUser {
+    role: string;
   }
 }
 declare module 'next-auth/jwt' {
   interface JWT {
-    role?: 'admin' | 'user' | 'mock' | 'codeView';
+    role?: string;
   }
 }
