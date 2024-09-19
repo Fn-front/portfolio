7255b76fac9ef7ec3ee99c9fa753385f929d4ddb
Fn-front <reservoir-station@outlook.jp>
2024-09-19 21:13:34 +0900

    add: types next-auth.d.ts

--- src/functions/types/next-auth.d.ts
+++ src/functions/types/next-auth.d.ts
@@ -1,5 +1,12 @@
 import 'next-auth/jwt';
 
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      role: 'admin' | 'user' | 'mock' | 'codeView';
+    } & DefaultSession['user'];
+  }
+}
 declare module 'next-auth/jwt' {
   interface JWT {
     role?: 'admin' | 'user' | 'mock' | 'codeView';
