3fc8b2808ab4ca2d7d649bca07c80e299112659a
Fn-front <reservoir-station@outlook.jp>
2024-09-20 21:29:56 +0900

    add: login

--- src/features/Ui/Header/components/userInfo/index.tsx
+++ src/features/Ui/Header/components/userInfo/index.tsx
@@ -1,3 +1,6 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 type Props = {
   info: {
     name: string
@@ -9,10 +12,31 @@ type Props = {
 
 export const UiHeaderLogout = (props: Props) => {
   const { info } = props
+
+  const handleNameLength = (char: string) => {
+    const maxLength = 5
+    const length = char.length
+
+    if (length > maxLength) {
+      char = char.slice(0, maxLength) + '...'
+    }
+
+    return char
+  }
+
   return (
-    <>
-      <p>ログアウト</p>
-    </>
+    <div className='c_header_user_info'>
+      <div className='c_header_user_info_name'>
+        <AccountCircleIcon fontSize='large' sx={{ fontSize: '3rem' }} />
+        <div className='c_header_user_info_name_container'>
+          <span className='c_header_user_info_name_main'>{handleNameLength(info.name)}</span> さん
+        </div>
+        <ArrowDropDownIcon fontSize='large' />
+      </div>
+      <div className='c_header_user_info_drop'>
+        <button className='c_button'>ログアウト</button>
+      </div>
+    </div>
   )
 }
 
