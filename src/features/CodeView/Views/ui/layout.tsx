9f6f518f502031818c6289ef6bd285f4cfc2e222
Fn-front <reservoir-station@outlook.jp>
2024-09-28 13:44:56 +0900

    debug: test2

--- src/features/CodeView/Views/ui/layout.tsx
+++ src/features/CodeView/Views/ui/layout.tsx
@@ -3,12 +3,12 @@ import { viewsDir } from '@/functions/constants/paths';
 import CodeViewCardList from '../../CardList';
 
 export default async function ViewUiForm() {
-  const filePaths = await getFileDir(viewsDir + 'codeView/ui/layout/');
+  // const filePaths = await getFileDir(viewsDir + 'codeView/ui/layout/');
 
   return (
     <>
       <h2 className='c_h2 u_mt32'>Layout</h2>
-      <CodeViewCardList editFilePaths={filePaths} />
+      {/* <CodeViewCardList editFilePaths={filePaths} /> */}
     </>
   );
 }
