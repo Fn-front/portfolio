56491b234fe1ee8baaf28c3d7cb68d0cb49595b1
Fn-front <reservoir-station@outlook.jp>
2024-09-28 13:53:11 +0900

    debug: test4

--- src/functions/hooks/system/getFileDir/index.ts
+++ src/functions/hooks/system/getFileDir/index.ts
@@ -4,19 +4,19 @@ import recursiveReaddir from 'recursive-readdir';
 
 export const getFileDir = async (dir: string) => {
   const files = await recursiveReaddir(dir);
-  let pathnames;
-  if (files) {
-    const deleteDir = 'src/app/';
-    pathnames = files.map((file: string) => {
-      const pathname = file
-        .replace(deleteDir, '')
-        .replace(/.[jt]sx?$/, '')
-        .replace('page', '');
-      return pathname;
-    });
-    // .filter((file: string) => !/^_.+/.test(file))
-    // .filter((file: string) => file !== "index")
-  }
+  let pathnames = files;
+  // if (files) {
+  //   const deleteDir = 'src/app/';
+  //   pathnames = files.map((file: string) => {
+  //     const pathname = file
+  //       .replace(deleteDir, '')
+  //       .replace(/.[jt]sx?$/, '')
+  //       .replace('page', '');
+  //     return pathname;
+  //   });
+  //   // .filter((file: string) => !/^_.+/.test(file))
+  //   // .filter((file: string) => file !== "index")
+  // }
 
   return pathnames;
 };
