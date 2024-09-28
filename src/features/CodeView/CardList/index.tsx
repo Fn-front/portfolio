e00bbe16c6af147f18a542b367638e5f5b448294
Fn-front <reservoir-station@outlook.jp>
2024-09-28 15:26:24 +0900

    fix: 戻し

--- src/features/CodeView/CardList/index.tsx
+++ src/features/CodeView/CardList/index.tsx
@@ -6,12 +6,12 @@ type Props = {
 };
 
 export const CodeViewCardList = (props: Props) => {
-  // const { editFilePaths } = props;
+  const { editFilePaths } = props;
 
   return (
     <div className='p_code_view_card_list u_mt16'>
       <ul className='c_card_list c_card_list_dark'>
-        {/* {editFilePaths?.map((item: string, index) => {
+        {editFilePaths?.map((item: string, index) => {
           return (
             <li key={index} className='c_card_list_item'>
               <div className='c_card_list_item_main'>
@@ -34,7 +34,7 @@ export const CodeViewCardList = (props: Props) => {
               </div>
             </li>
           );
-        })} */}
+        })}
       </ul>
     </div>
   );
--- src/functions/hooks/system/getFileDir/index.ts
+++ src/functions/hooks/system/getFileDir/index.ts
@@ -1,38 +1,23 @@
 /* eslint-disable */
 // @ts-ignore
-// import recursiveReaddir from 'recursive-readdir';
-const fs = require('fs');
-const path = require('path');
+import recursiveReaddir from 'recursive-readdir';
 
 export const getFileDir = async (dir: string) => {
-  // const files = await recursiveReaddir(dir);
-  const readdirRecursively = (dir: string, files = []) => {
-    const dirents = fs.readdirSync(path.resolve(dir), { withFileTypes: true });
-    const dirs = [];
-    for (const dirent of dirents) {
-      if (dirent.isDirectory()) dirs.push(`${dir}/${dirent.name}`);
-      /// @ts-ignore
-      if (dirent.isFile()) files.push(`${dir}/${dirent.name}`);
-    }
-    for (const d of dirs) {
-      files = readdirRecursively(d, files);
-    }
-    return files;
-  };
+  const files = await recursiveReaddir(dir);
 
-  // let pathnames = ['aaa'];
-  // if (files) {
-  //   const deleteDir = 'src/app/';
-  //   pathnames = files.map((file: string) => {
-  //     const pathname = file
-  //       .replace(deleteDir, '')
-  //       .replace(/.[jt]sx?$/, '')
-  //       .replace('page', '');
-  //     return pathname;
-  //   });
-  //   // .filter((file: string) => !/^_.+/.test(file))
-  //   // .filter((file: string) => file !== "index")
-  // }
+  let pathnames;
+  if (files) {
+    const deleteDir = 'src/app/';
+    pathnames = files.map((file: string) => {
+      const pathname = file
+        .replace(deleteDir, '')
+        .replace(/.[jt]sx?$/, '')
+        .replace('page', '');
+      return pathname;
+    });
+    // .filter((file: string) => !/^_.+/.test(file))
+    // .filter((file: string) => file !== "index")
+  }
 
-  return readdirRecursively(dir);
+  return pathnames;
 };
