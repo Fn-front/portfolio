26dd18db5487cf62037929942221b37d22c66492
Fn-front <reservoir-station@outlook.jp>
2024-09-29 09:23:17 +0900

    Revert "test: debug"
    This reverts commit 0a7eb451222e26e99637b835e30ed087f13e7a0a.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/styles/': ['./src/styles/**/*'],
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
