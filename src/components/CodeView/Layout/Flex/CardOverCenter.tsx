80ce0c1497cf31ec79a939ba52d05d35f8c1e6fd
Fn-front <reservoir-station@outlook.jp>
2024-11-04 19:53:51 +0900

    update: 要素調整

--- src/components/CodeView/Layout/Flex/CardOverCenter.tsx
+++ src/components/CodeView/Layout/Flex/CardOverCenter.tsx
@@ -69,93 +69,95 @@ const Default = async () => {
       </ComponentWrapper>
       <ComponentWrapper styleCode={content}>
         <div className='flex_overflow_wrapper'>
-          <ul className='c_card_list c_card_list_dark'>
-            <li className='c_card_list_item'>
-              <div className='c_card_list_item_main'>
-                <div className='c_next_image_wrapper'>
-                  <Image
-                    src='/assets/img/1920x1080.png'
-                    alt='Sample Image'
-                    className='c_next_image'
-                    sizes='100%'
-                    fill
-                    priority
-                  />
+          <div className='flex_overflow'>
+            <ul className='c_card_list c_card_list_dark'>
+              <li className='c_card_list_item'>
+                <div className='c_card_list_item_main'>
+                  <div className='c_next_image_wrapper'>
+                    <Image
+                      src='/assets/img/1920x1080.png'
+                      alt='Sample Image'
+                      className='c_next_image'
+                      sizes='100%'
+                      fill
+                      priority
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className='c_card_list_item_other'>
-                <p>ダミーテキスト</p>
-              </div>
-            </li>
-            <li className='c_card_list_item'>
-              <div className='c_card_list_item_main'>
-                <div className='c_next_image_wrapper'>
-                  <Image
-                    src='/assets/img/1920x1080.png'
-                    alt='Sample Image'
-                    className='c_next_image'
-                    sizes='100%'
-                    fill
-                    priority
-                  />
+                <div className='c_card_list_item_other'>
+                  <p>ダミーテキスト</p>
                 </div>
-              </div>
-              <div className='c_card_list_item_other'>
-                <p>ダミーテキスト</p>
-              </div>
-            </li>
-            <li className='c_card_list_item'>
-              <div className='c_card_list_item_main'>
-                <div className='c_next_image_wrapper'>
-                  <Image
-                    src='/assets/img/1920x1080.png'
-                    alt='Sample Image'
-                    className='c_next_image'
-                    sizes='100%'
-                    fill
-                    priority
-                  />
+              </li>
+              <li className='c_card_list_item'>
+                <div className='c_card_list_item_main'>
+                  <div className='c_next_image_wrapper'>
+                    <Image
+                      src='/assets/img/1920x1080.png'
+                      alt='Sample Image'
+                      className='c_next_image'
+                      sizes='100%'
+                      fill
+                      priority
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className='c_card_list_item_other'>
-                <p>ダミーテキスト</p>
-              </div>
-            </li>
-            <li className='c_card_list_item'>
-              <div className='c_card_list_item_main'>
-                <div className='c_next_image_wrapper'>
-                  <Image
-                    src='/assets/img/1920x1080.png'
-                    alt='Sample Image'
-                    className='c_next_image'
-                    sizes='100%'
-                    fill
-                    priority
-                  />
+                <div className='c_card_list_item_other'>
+                  <p>ダミーテキスト</p>
                 </div>
-              </div>
-              <div className='c_card_list_item_other'>
-                <p>ダミーテキスト</p>
-              </div>
-            </li>
-            <li className='c_card_list_item'>
-              <div className='c_card_list_item_main'>
-                <div className='c_next_image_wrapper'>
-                  <Image
-                    src='/assets/img/1920x1080.png'
-                    alt='Sample Image'
-                    className='c_next_image'
-                    sizes='100%'
-                    fill
-                    priority
-                  />
+              </li>
+              <li className='c_card_list_item'>
+                <div className='c_card_list_item_main'>
+                  <div className='c_next_image_wrapper'>
+                    <Image
+                      src='/assets/img/1920x1080.png'
+                      alt='Sample Image'
+                      className='c_next_image'
+                      sizes='100%'
+                      fill
+                      priority
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className='c_card_list_item_other'>
-                <p>ダミーテキスト</p>
-              </div>
-            </li>
-          </ul>
+                <div className='c_card_list_item_other'>
+                  <p>ダミーテキスト</p>
+                </div>
+              </li>
+              <li className='c_card_list_item'>
+                <div className='c_card_list_item_main'>
+                  <div className='c_next_image_wrapper'>
+                    <Image
+                      src='/assets/img/1920x1080.png'
+                      alt='Sample Image'
+                      className='c_next_image'
+                      sizes='100%'
+                      fill
+                      priority
+                    />
+                  </div>
+                </div>
+                <div className='c_card_list_item_other'>
+                  <p>ダミーテキスト</p>
+                </div>
+              </li>
+              <li className='c_card_list_item'>
+                <div className='c_card_list_item_main'>
+                  <div className='c_next_image_wrapper'>
+                    <Image
+                      src='/assets/img/1920x1080.png'
+                      alt='Sample Image'
+                      className='c_next_image'
+                      sizes='100%'
+                      fill
+                      priority
+                    />
+                  </div>
+                </div>
+                <div className='c_card_list_item_other'>
+                  <p>ダミーテキスト</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </ComponentWrapper>
     </>
