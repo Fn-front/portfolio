2f85e9a260f3a9b48d883ee2d6926457ba662b6d
Fn-front <reservoir-station@outlook.jp>
2024-11-04 19:36:21 +0900

    update: css read

--- src/components/CodeView/Layout/Flex/CardOverCenter.tsx
+++ src/components/CodeView/Layout/Flex/CardOverCenter.tsx
@@ -3,8 +3,8 @@ import { getFile } from '@/features/ReadFile';
 import ComponentWrapper from '@/components/Layout/ComponentWrapper';
 
 const Default = async () => {
-  const contentPath = './src/styles/components/card/';
-  const contentFile = '_dark.scss';
+  const contentPath = './src/styles/components/codeView/flex/';
+  const contentFile = '_overflow_center.scss';
   const content: string = await getFile(contentPath, contentFile);
 
   return (
