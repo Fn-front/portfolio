31ff3afe1b14cc01f7e30bd9840880bf9e99cdba
Fn-front <reservoir-station@outlook.jp>
2024-11-04 18:55:57 +0900

    add: component flex

---
+++ src/components/CodeView/Layout/Flex/index.tsx
@@ -0,0 +1,12 @@
+// import Default from './CardListDefault';
+// import Dark from './CardListDark';
+
+const Flex = () => {
+  return (
+    <>
+      <p>あああ</p>
+    </>
+  );
+};
+
+export default Flex;
