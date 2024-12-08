aff821c36a5b53899e77c457c46f8c4122464256
Fn-front <reservoir-station@outlook.jp>
2024-12-08 16:26:51 +0900

    add: route 記事一覧取得

---
+++ src/app/api/profile/achievement/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+// データの型定義
+interface Achievement {
+  id: string;
+  title: string;
+  content: string;
+  imagePath: string;
+}
+
+// 全記事を取得するAPI
+export async function GET() {
+  try {
+    const jsonDirectory = path.join(process.cwd(), 'src', 'lib', 'data');
+    const fileContents = await fs.readFile(
+      jsonDirectory + '/articles.json',
+      'utf8',
+    );
+
+    const achievements: Achievement[] = JSON.parse(fileContents);
+
+    const achievementsWithAbsolutePaths = achievements.map((achievement) => ({
+      ...achievement,
+      imagePath: `/assets/img/achievements/${achievement.imagePath}`,
+    }));
+
+    return NextResponse.json(achievementsWithAbsolutePaths);
+  } catch (error) {
+    return NextResponse.json(
+      { error: `記事を取得できませんでした ${error}` },
+      { status: 500 },
+    );
+  }
+}
