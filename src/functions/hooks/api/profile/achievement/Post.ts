2f33009bb797a307bfe03ff45d1aad68a692d5eb
Fn-front <reservoir-station@outlook.jp>
2024-12-08 19:09:33 +0900

    remove

--- src/functions/hooks/api/profile/achievement/Post.ts
+++ src/functions/hooks/api/profile/achievement/Post.ts
@@ -1,48 +1,48 @@
-'use client';
+// 'use client';
 
-import useSWR from 'swr';
-import axios, { isAxiosError } from 'axios';
-import { AchievementDetailProps, Achievement } from './type';
+// import useSWR from 'swr';
+// import axios, { isAxiosError } from 'axios';
+// import { AchievementDetailProps, Achievement } from './type';
 
-interface UseGetDetailAchievementReturn {
-  data: Achievement | undefined;
-  error: Error | undefined;
-  isLoading: boolean;
-  mutate: () => Promise<Achievement | undefined>;
-}
+// interface UseGetDetailAchievementReturn {
+//   data: Achievement | undefined;
+//   error: Error | undefined;
+//   isLoading: boolean;
+//   mutate: () => Promise<Achievement | undefined>;
+// }
 
-export const useGetDetailAchievement = ({
-  id,
-}: AchievementDetailProps): UseGetDetailAchievementReturn => {
-  const fetcher = async (url: string) => {
-    try {
-      const response = await axios.post<Achievement>(url, {
-        id,
-      });
-      return response.data;
-    } catch (error) {
-      if (isAxiosError(error)) {
-        throw new Error(
-          error.response?.data?.message || 'データの取得に失敗しました',
-        );
-      }
-      throw error;
-    }
-  };
+// export const useGetDetailAchievement = ({
+//   id,
+// }: AchievementDetailProps): UseGetDetailAchievementReturn => {
+//   const fetcher = async (url: string) => {
+//     try {
+//       const response = await axios.post<Achievement>(url, {
+//         id,
+//       });
+//       return response.data;
+//     } catch (error) {
+//       if (isAxiosError(error)) {
+//         throw new Error(
+//           error.response?.data?.message || 'データの取得に失敗しました',
+//         );
+//       }
+//       throw error;
+//     }
+//   };
 
-  const { data, error, isLoading, mutate } = useSWR(
-    id ? `/api/profile/achievement/${id}` : null,
-    fetcher,
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    },
-  );
+//   const { data, error, isLoading, mutate } = useSWR(
+//     id ? `/api/profile/achievement/${id}` : null,
+//     fetcher,
+//     {
+//       revalidateOnFocus: false,
+//       revalidateOnReconnect: false,
+//     },
+//   );
 
-  return {
-    data,
-    error,
-    isLoading,
-    mutate,
-  };
-};
+//   return {
+//     data,
+//     error,
+//     isLoading,
+//     mutate,
+//   };
+// };
