cd5d710d65159d251de6f6d5f436c2f1a0afbf3c
Fn-front <reservoir-station@outlook.jp>
2025-08-24 18:16:17 +0900

    delete: hooks

--- src/functions/hooks/system/context/systemMessage.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const SystemMessageContext = React.createContext(
-  {} as {
-    setMessage: React.Dispatch<React.SetStateAction<string>>;
-    setType: React.Dispatch<React.SetStateAction<string>>;
-    setVisible: React.Dispatch<React.SetStateAction<boolean>>;
-  },
-);
