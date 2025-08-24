6aa3d0d24a1b1e76938ea9245bf693be3ef1d965
Fn-front <reservoir-station@outlook.jp>
2025-08-24 18:41:01 +0900

    fix: layout

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -4,12 +4,8 @@ export default function ProfileLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className='l_global_container l_global_container_no_header'>
-      <main className='l_main l_main_no_sidebar'>
-        <div className='l_container'>
-          {children}
-        </div>
-      </main>
-    </div>
+    <>
+      {children}
+    </>
   )
 }
