998b7b1367a74ac6ef161717e0c303bc36bd0797
Fn-front <reservoir-station@outlook.jp>
2025-08-27 17:48:01 +0900

    add: constants skills

---
+++ src/functions/constants/skills.ts
@@ -0,0 +1,42 @@
+import { TECHNICAL_SKILLS, OTHERS_LIBRARY_SKILLS, TOOL_SKILLS } from './skillsData';
+
+export const SKILL_TAGS = {
+  TECHNICAL: {
+    REACT: 'React',
+    NEXT_JS: 'Next.js',
+    VUE_JS: 'Vue.js',
+    TYPESCRIPT: 'TypeScript',
+    TAILWIND: 'Tailwind',
+    HTML_PUG_EJS: 'HTML/Pug/Ejs',
+    CSS_SASS: 'CSS/Sass',
+    JAVASCRIPT: 'JavaScript',
+    PYTHON: 'Python',
+    LARAVEL: 'Laravel',
+    MOVABLE_TYPE: 'MovableType',
+    WORDPRESS: 'WordPress',
+    VITE: 'Vite',
+    WEBPACK: 'Webpack',
+    GULP: 'Gulp',
+  },
+  OTHERS_LIBRARY: {
+    RADIX_UI: 'Radix UI',
+    SHADCN_UI: 'shadcn/ui',
+    REACT_HOOK_FORM: 'react-hook-form',
+    ZOD: 'zod',
+    ESLINT: 'ESLint',
+    PRETTIER: 'Prettier',
+    STYLELINT: 'Stylelint',
+    MARKUPLINT: 'Markuplint',
+  },
+  TOOL: {
+    PHOTOSHOP: 'PhotoShop',
+    ILLUSTRATOR: 'Illustrator',
+    XD: 'XD',
+    FIGMA: 'Figma',
+    GITHUB: 'Github',
+    VSCODE: 'VSCode',
+    BACKLOG: 'Backlog',
+  },
+};
+
+export { TECHNICAL_SKILLS, OTHERS_LIBRARY_SKILLS, TOOL_SKILLS };
